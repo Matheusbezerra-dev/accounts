37d6f885a894f36ad532abc22444ca742230bd94
Matheus Bezerra <mabezerra2@gmail.com>
2023-01-24 19:17:32 -0400

    instalando as dependencias

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// const inquirer: any= require("inquirer");
+// const chalk: any= require('chalk');
+//modulo internos
+const fs = require('fs');
+console.log('iniciando o Account');
---
+++ index.ts
@@ -0,0 +1,11 @@
+//modulos externos
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+// const inquirer: any= require("inquirer");
+
+// const chalk: any= require('chalk');
+
+//modulo internos
+const fs: any= require('fs');
+
+console.log('iniciando o Account');
