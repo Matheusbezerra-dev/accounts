0afa846328d3221776ae70ad70aa66a0a2b53642
Matheus Bezerra <mabezerra2@gmail.com>
2023-01-24 20:26:48 -0400

    configurando e começando a fazer a regra de negocio

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-// const inquirer: any= require("inquirer");
+const inquirer_1 = __importDefault(require("inquirer"));
+// const inquirer: any= require("inquirer");sdsadsa
 // const chalk: any= require('chalk');
 //modulo internos
-const fs = require('fs');
-console.log('iniciando o Account');
+// const fs: any= require('fs');
+const operation = () => {
+    inquirer_1.default.prompt([{
+            type: 'list',
+            name: 'action',
+            message: 'O que você deseja fazer?',
+            choices: [
+                'Criar conta',
+                'Consultar Saldo',
+                'Depositar',
+                'Sacar',
+                'Sair',
+            ],
+        }])
+        .then((answer) => {
+        const action = answer['action'];
+    })
+        .catch((err) => console.log(err));
+};
+operation();
--- index.ts
+++ index.ts
@@ -1,11 +1,31 @@
 //modulos externos
 import chalk from 'chalk';
 import inquirer from 'inquirer';
-// const inquirer: any= require("inquirer");
+import fs from 'fs'
+// const inquirer: any= require("inquirer");sdsadsa
 
 // const chalk: any= require('chalk');
 
 //modulo internos
-const fs: any= require('fs');
+// const fs: any= require('fs');
 
-console.log('iniciando o Account');
+const operation = () => {
+  inquirer.prompt([{
+    type: 'list',
+    name: 'action',
+    message: 'O que você deseja fazer?',
+    choices: [
+      'Criar conta',
+      'Consultar Saldo',
+      'Depositar',
+      'Sacar',
+      'Sair',
+    ],
+  }])
+  .then((answer) => {
+    const action: string = answer['action']
+  })
+  .catch((err) => console.log(err));
+}
+
+operation()
